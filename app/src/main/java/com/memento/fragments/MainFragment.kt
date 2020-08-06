534097cacf3b75597718e1d051394ccb9e3c4e6f
FernandoH-G <fherrera2018@gmail.com>
2020-08-06 01:01:52 -0700

    Added content to main fragment.

--- app/src/main/java/com/memento/fragments/MainFragment.kt
+++ app/src/main/java/com/memento/fragments/MainFragment.kt
@@ -1,6 +1,27 @@
 package com.memento.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.memento.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private var tmbBinding: FragmentMainBinding? = null
+    private val mBinding get() = tmbBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        tmbBinding = FragmentMainBinding.inflate(inflater,container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroyView() {
+        tmbBinding = null
+        super.onDestroyView()
+    }
 }
